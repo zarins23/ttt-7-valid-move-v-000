a09b940ea587b4b32cdc0a0ea01b6c6eec8ef6a2
Zarin Shahab <zarins23@gmail.com>
2018-02-17 02:17:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,10 +11,8 @@ end
 def position_taken?(board, index)
    if board[index] == " " || board[index] == "" || board[index] == nil
      false
-   elsif board[index] == "X" || board[index] == "O"
-     true
    else
-     false
+     true
    end
 end
 
