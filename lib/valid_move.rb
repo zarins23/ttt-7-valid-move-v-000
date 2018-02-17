240e24687588670d9a20073254a414723bfd88b1
Zarin Shahab <zarins23@gmail.com>
2018-02-17 02:08:44 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     false
-  elsif valid_move?(board, index).between?(0, 8)
+  elsif position_taken?(board,index).between?(0, 8)
     true
   else
     false
